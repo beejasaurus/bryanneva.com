e8792b259e5e0828009e87d067be157e86111240
Bryan Neva <bryan.neva@gmail.com>
2016-09-24 17:22:31 -0400

    Add toasts, modals, and other event listeners

--- src/index.js
+++ src/index.js
@@ -22,3 +22,51 @@ $('.portfolio-category-filter').click((e) => {
 
     $li.toggleClass('active');
 });
+
+$('.chip .close').click((e) => {
+    const $this = $(e.currentTarget);
+    $this.parent().remove();
+});
+
+$('.contact .submit').click((e) => {
+    console.log('Submit');
+    
+    e.preventDefault();
+   
+    const $toasts = $('.toast');    
+    $toasts.fadeIn(400);
+
+    setTimeout(() => {
+        $toasts.fadeOut(400);
+    }, 3000);
+});
+
+$('.portfolio-header h3, .portfolio-actions a').click((e) => {
+
+    e.preventDefault();
+
+    const $modals = $('.modal-container');
+    
+    $modals.fadeIn(200);
+    activateOverlay();
+});
+
+$('.modal-close, .overlay').click((e) => {
+
+    e.preventDefault();
+
+    const $modals = $('.modal-container');
+    
+    deactivateOverlay();   
+    $modals.fadeOut(100);
+});
+
+function activateOverlay() {
+    const $overlay = $('.overlay');
+    $overlay.show();
+}
+
+function deactivateOverlay() {
+    const $overlay = $('.overlay');
+    $overlay.hide();
+}
